d607225ca99550bfb528237cf9af413ede8f0915
Marcelo Andrade <meandrade@Marcelos-MacBook-Pro.local>
2013-10-31 16:44:53 -0200

    Restructuring JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-$(function() {
-      
-        $(' #da-thumbs > li ').each( function() { $(this).hoverdir(); } );
+var Main = (function() {
+  return {
+    init: function() {
+      this.grid_hovers()
+    },
 
-      });
+    grid_hovers: function(){
+      $(' #da-thumbs > li ').each( function() { $(this).hoverdir(); } );
+    }
+  }
+})();
+
+$(document).ready(function(){
+  Main.init()
+});
