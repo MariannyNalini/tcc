116cf361e57784599afc380a0ccf4814928151ec
Marcelo Andrade <meandrade@Marcelos-MacBook-Pro.local>
2013-10-31 17:40:05 -0200

    Fixed menu is done

--- js/shared/main.js
+++ js/shared/main.js
@@ -1,13 +1,30 @@
 // FUNÇÕES GERAIS DA APLICAÇÃO, QUE SERAM USADAS EM TODAS AS TELAS
 
-var Main = (function() {
-  return {
-    init: function() {
-      console.log('init app') 
-    }
+function Main() {
+  var menu = $('nav.interna')
+
+  this.init = function() {
+    set_scroll_trigger()
   }
-})();
+
+  function set_scroll_trigger(){
+    $( window ).scroll(function(){
+      fix_menu()
+    })
+  }
+
+  function fix_menu(){
+    var scroll_top = $( window ).scrollTop()
+
+    menu.removeClass('fixed')
+
+    if( ( scroll_top - menu.offset().top ) >= 0 )
+      menu.addClass('fixed')
+  }
+
+}
 
 $(document).ready(function(){
-  Main.init()
+  main = new Main()
+  main.init()
 });
