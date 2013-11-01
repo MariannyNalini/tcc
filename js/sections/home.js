22b31bab0ee9af5b2cd81f828061dcdc92b34923
Marcelo Andrade <meandrade@Marcelos-MacBook-Pro.local>
2013-11-01 12:49:47 -0200

    Tree full size

--- js/sections/home.js
+++ js/sections/home.js
@@ -1,17 +1,34 @@
 // FUNÇÕES GERAIS DA HOME DA APLICAÇÃO
 
-var Home = (function() {
-  return {
-    init: function() {
-      this.grid_hovers()
-    },
+function Home() {
+  var menu = $('nav.interna')
 
-    grid_hovers: function(){
-      $(' #da-thumbs > li ').each( function() { $(this).hoverdir(); } );
-    }
+  this.init = function() {
+    full_tree_trigger()
   }
-})();
+
+  function full_tree_trigger(){
+    full_tree()
+
+    $(window).resize( full_tree )
+  }
+
+  function full_tree(){
+    var window_w = $(window).width();
+    var wrapper_w = 960;
+    var tree_cont_w = ( window_w - wrapper_w ) / 2;
+
+    var tree_cont = $('header .tree-cont');
+
+    tree_cont.css({
+      width: tree_cont_w,
+      left: -(tree_cont_w)
+    })
+  }
+
+}
 
 $(document).ready(function(){
-  Home.init()
+  home = new Home()
+  home.init()
 });
