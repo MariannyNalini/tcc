71d8f5489b693809728b79015a49547920ebfa73
MariannyNalini <marianny.nalini@gmail.com>
2013-12-02 17:13:11 -0200

    Interna.

--- wp-content/themes/okida/header.php
+++ wp-content/themes/okida/header.php
@@ -7,8 +7,13 @@
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    
+    
+    <?php if(is_category('Portfolio')) :?>
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/sections/interna.css" media="screen" />
+    <?php endif;?>
+		
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
