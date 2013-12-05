02fa75b970a6a0a5ad6787514cf6752b5cab593c
MariannyNalini <marianny.nalini@gmail.com>
2013-12-05 04:13:34 -0200

    Tree menu.

---
+++ wp-content/themes/okida/header-tree.php
@@ -0,0 +1,47 @@
+<div class="wrapper">
+  <span class="tree-cont"></span>
+    <nav class="tree">
+      <ul>
+        <li class="animate tossing" id="portfolio">
+          <a href="#section-portfolio">
+            <span>Portfolio</span>
+          </a>
+        </li>
+        <li class="animate tossing" id="music-and-theater">
+          <a href="#section-musicandtheater" class="menu_button">
+            <span>Música e Teatro</span>
+          </a>
+        </li>
+        <li class="animate tossing" id="blog">
+          <a href="#section-blog" class="menu_button">
+            <span>Blog</span>
+          </a>
+        </li>
+        <li class="animate tossing" id="about">
+          <a href="#section-about" class="menu_button">
+            <span>Sobre</span>
+          </a>
+        </li>
+        <li class="animate tossing" id="contact">
+          <a href="#section-contact" class="menu_button">
+            <span>Contato</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <ul class="social">
+      <li>
+        <a href="http://www.pinterest.com/marciaokida/" class="pinterest" target="_blank">Pinterest</a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/marciaokida" class="facebook" target="_blank">Facebook</a>
+            </li>
+            <li class="twitter">
+              <a href="https://twitter.com/marciaokida" class="twitter" target="_blank">Twitter</a>
+            </li>
+          </ul>
+</div>  
+    
+<?php
+  require 'nav.php';
+?>
