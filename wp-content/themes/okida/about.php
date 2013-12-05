30c29fc0f5808871424f2db44273307974fb8a18
MariannyNalini <marianny.nalini@gmail.com>
2013-12-05 04:13:24 -0200

    About page.

---
+++ wp-content/themes/okida/about.php
@@ -0,0 +1,46 @@
+<section id="about">
+  <a name="section-about"></a>
+  <div class="wrapper">
+  <h2 class="title">Sobre</h1>
+
+  <?php if (have_posts()): ?>
+
+    <?php
+      $temp = $wp_query;
+      $wp_query= null;
+      $wp_query = new WP_Query();
+      $wp_query->query('pagename=sobre');
+    ?>
+
+    <article id="okida">
+
+      <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+
+        <li id="post-<?php the_ID(); ?>">
+          <a href="<?php the_permalink() ?>" rel="bookmark">
+          <?php
+            if ( has_post_thumbnail() ) {
+              the_post_thumbnail();
+            }
+            else {?>
+              <img src="<?php bloginfo('template_directory'); ?>/img/thumb.jpg">
+            <?}?>
+          ?>
+          <div>
+            <span><?php the_title(); ?></span>
+          </div>      
+          </a>
+        </li>
+
+        <?php comments_template(); // Get wp-comments.php template ?>
+
+      <?php endwhile; ?>
+
+    </ul>
+
+  <?php endif; ?>
+
+  <?php $wp_query = null; $wp_query = $temp;?>
+  <div class="clear"></div>
+</div>
+</section>
