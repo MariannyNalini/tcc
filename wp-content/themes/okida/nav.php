d594a16836fcd71d84602b6192e9461355750b1c
MariannyNalini <marianny.nalini@gmail.com>
2013-12-05 04:14:25 -0200

    Nav.

---
+++ wp-content/themes/okida/nav.php
@@ -0,0 +1,22 @@
+<nav class="interna">
+  <div class="wrapper">
+    <h2 class="title"></h2>
+    <ul>
+      <li>
+        <a href="#section-portfolio" class="portfolio menu_button">Portfolio</a>
+      </li>
+      <li>
+        <a href="#section-musicandtheater" class="music-and-theater menu_button">Música e Teatro</a>
+      </li>
+      <li>
+        <a href="#section-blog" class="blog menu_button">Blog</a>
+      </li>
+      <li>
+        <a href="#section-about" class="about menu_button">Sobre</a>
+      </li>
+      <li>
+        <a href="#section-contact" class="contact menu_button">Contato</a>
+      </li>
+    </ul>
+  </div>
+</nav>
