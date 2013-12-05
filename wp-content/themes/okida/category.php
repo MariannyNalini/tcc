609ffa3d1a2c056c41180a82dd7a5134626ed1f0
MariannyNalini <marianny.nalini@gmail.com>
2013-12-05 17:51:20 -0200

    Fix categories.

--- wp-content/themes/okida/category.php
+++ wp-content/themes/okida/category.php
@@ -1,10 +1,10 @@
 <?php if (is_category( 'portfolio' )) { 
-		require 'category-portfolio.php'
+		require 'category-portfolio.php';
 	} else if (is_category( 'musica-e-teatro')){ 
-		require 'category-musicateatro.php'
-	else if (is_category('blog')){
-		require 'category-blog.php'
+		require 'category-musicateatro.php';
+	} else if (is_category('blog')){
+		require 'category-blog.php';
 	} else{
-		require 'category.php'
+		require 'category.php';
 	}
 ?>
